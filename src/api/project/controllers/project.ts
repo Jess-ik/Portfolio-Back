cf4b7ae8f63fd8a13a8aabb9aec51c737003d3b1
Jess <jessica.louvel@gmail.com>
2023-08-09 14:26:59 +0200

    first commit

---
+++ src/api/project/controllers/project.ts
@@ -0,0 +1,25 @@
+/**
+ * project controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::project.project', ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.db.query('api::project.project').findOne({
+            where: { slug: id },
+            populate: ["heroImage", "showcaseImage", "tools", "gallery"],
+          
+            
+
+
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+        
+        
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
