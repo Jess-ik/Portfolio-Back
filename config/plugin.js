a3dda7b4824c797c089b05ff41fad21324652e44
Jess <jessica.louvel@gmail.com>
2023-08-27 10:59:14 +0200

    add data import plugin

--- config/plugin.js
+++ config/plugin.js
@@ -19,4 +19,11 @@ module.exports = ({ env }) => ({
       },
     },
     // ...
+    'import-export-entries': {
+      enabled: true,
+      config: {
+        // See `Config` section.
+      },
+  },
+    // ...
   });
