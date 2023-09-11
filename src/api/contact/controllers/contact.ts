61431f579d541b276ac957c14447b3cb2530de60
Jess <jessica.louvel@gmail.com>
2023-09-11 12:27:50 +0200

    edit email

--- src/api/contact/controllers/contact.ts
+++ src/api/contact/controllers/contact.ts
@@ -4,22 +4,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::contact.contact', ({ strapi }) => ({
-    async create(event: any) {
-    
-        const { result } = event;
-  
-    try {
-      await strapi.plugins['email'].services.email.send({
-        to: 'jessica.louvel@gmail.com',
-        from: 'contact@jess-louvel.com',
-        replyTo: `${result.email}`,
-        subject: `Nouvelle demande de contact de ${result.name} sur jess-louvel`,
-        text: `${result.message}`,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-    }
-})
-  );
+export default factories.createCoreController('api::contact.contact');
