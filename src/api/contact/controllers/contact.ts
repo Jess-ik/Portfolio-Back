c2b687935e793f542c28a73d799444ce1fbf77e8
Jess <jessica.louvel@gmail.com>
2023-10-23 09:25:06 +0200

    fixing

--- src/api/contact/controllers/contact.ts
+++ src/api/contact/controllers/contact.ts
@@ -8,23 +8,22 @@ const axios = require('axios');
 export default factories.createCoreController('api::contact.contact', ({ strapi }) => ({
     async create(ctx) {
         // Récupérer les données du formulaire soumis par l'utilisateur
-
         try {
             const { name,
                 email,
                 message
             } = ctx.request.body.data;
-            const {recaptcha} = ctx.request.body;
-            console.log(recaptcha)
+            const { recaptcha } = ctx.request.body;
+            //console.log(recaptcha)
 
             // Vérifier la clé reCAPTCHA
             const secretKey = process.env.RECAPTCHA_SECRET_KEY;
-            console.log(secretKey)
+            //console.log(secretKey)
 
-            console.log(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`);
+            //console.log(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`);
             const response = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${recaptcha}`);
             const { success } = response.data;
-            console.log(response)
+            //console.log(response)
             if (!success) {
                 // La vérification de la clé reCAPTCHA a échoué
                 ctx.response.status = 400;
@@ -34,23 +33,23 @@ export default factories.createCoreController('api::contact.contact', ({ strapi 
 
 
 
-
+            // Creation d'une entry de Contact
             await super.create(ctx);
 
             // Retourner une réponse de succès
             ctx.response.status = 200;
             ctx.response.body = { message: "Votre message a été envoyé avec succès." };
 
+            // Envoie mail de confirmation a l'auteur
+            await strapi.plugins['email'].services.email.send({
+                to: email,
+                from: 'contact@jess-louvel.com', // e.g. single sender verification in SendGrid
+                subject: "Your request on jess-louvel.com",
+                text: message, // Replace with a valid field ID:
+                html: 'Hi ' + name + ',<br/>Your message via jess-louvel.com contact form has been received.<br/>I will make sure to get back to you shortly.<br/><br/>Thank you for reaching out! <br/><br/>Best regards,<br/><br/>Jessica Louvel<br/>Front-end Developper<br/><a href="https://jess-louvel.com">https://jess-louvel.com</a><br/>____________<br/>Your message:<br/><br/><em>' + message + '<em>',
 
-               await strapi.plugins['email'].services.email.send({
-                  to: email,
-                   from: 'contact@jess-louvel.com', // e.g. single sender verification in SendGrid
-                   subject: "Your request on jess-louvel.com",
-                   text: message, // Replace with a valid field ID:
-                  html: 'Hi ' + name + ',<br/>Your message via jess-louvel.com contact form has been received.<br/>I will make sure to get back to you shortly.<br/><br/>Thank you for reaching out! <br/><br/>Best regards,<br/><br/>Jessica Louvel<br/>Front-end Developper<br/><a href="https://jess-louvel.com">https://jess-louvel.com</a><br/>____________<br/>Your message:<br/><br/><em>' + message + '<em>', 
-                  
-                 })
-
+            })
+            // Envoie mail de nouveau message
             await strapi.plugins['email'].services.email.send({
                 to: 'jessica.louvel@gmail.com',
                 from: 'contact@jess-louvel.com', // e.g. single sender verification in SendGrid
@@ -75,15 +74,8 @@ export default factories.createCoreController('api::contact.contact', ({ strapi 
 
             }
 
-
             return;
         }
-
-
     },
-
-
-
-
 }));
 
